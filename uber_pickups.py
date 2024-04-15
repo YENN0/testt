f657bf9b50887e94bb9d9dca53e9e8e948f8bb48
YENN0 <167049781+YENN0@users.noreply.github.com>
2024-04-15 11:38:51 +0800

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -4,7 +4,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 streamlit run uber_pickups.py
 
-DATE_COLUMN = 'date/time’
+DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/' 'streamlit-demo-data/uber-raw-data-sep14.csv.gz’)
 
 @st.cache_data 
