c6108b17743cffbead9918250db344662714f291
YENN0 <167049781+YENN0@users.noreply.github.com>
2024-06-04 22:46:26 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,62 +1,66 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-#工作列
+#Info.
 st.set_page_config(
     page_title="資料分析期末",
     layout = "wide",
     menu_items={
-        "About":"資料來源：[臺電官網](https://www.taipower.com.tw/tc/index.aspx)",
-        #"About":"https://www.taipower.com.tw/tc/index.aspx",
+        "About":"資料來源：[臺電官網](https://www.taipower.com.tw/tc/index.aspx)、[經濟部能源署](https://www.esist.org.tw/database/search?tab=%E9%9B%BB%E5%8A%9B%E7%B5%B1%E8%A8%88)",
     }
 )
 
-
+#Main
 def main():
     st.title('臺灣能源現況')
     st.write('410971105 顏詠璇 資料分析期末')
-    
+
     tab1, tab2 = st.tabs(["主介面", "臺灣能源政策目標"])
 
     with tab1:
-        df = pd.read_csv('https://raw.githubusercontent.com/YENN0/testt/main/capacity%20classification.csv')
-        dfshow = df.rename(columns={'年':'index'}).set_index('index')
-        st.subheader('裝置容量結構')
-        col1a, col2a = st.columns(2)
-        with col1a:
-            multiselected_columns = st.multiselect('選擇要顯示的列',dfshow.columns)
-        with col2a:
-             chart_type=st.selectbox('選擇圖表類型',['折線圖','柱狀圖'])
-        
-        if multiselected_columns:
-            df_selected = dfshow[multiselected_columns]
-            if chart_type == '折線圖':
-                st.line_chart(df_selected)
-            elif chart_type == '柱狀圖':
-                st.bar_chart(df_selected)
-        else:
-            if chart_type == '折線圖':
-                st.line_chart(dfshow)
-            elif chart_type == '柱狀圖':
-                st.bar_chart(dfshow)
+        col_main1, col_main2 = st.columns(2)
+        with col_main1:
+            df_capa = pd.read_csv('https://raw.githubusercontent.com/YENN0/testt/main/capacity%20classification.csv')
+            df_capa_show = df_capa.rename(columns={'年':'index'}).set_index('index')
+            st.subheader('裝置容量結構')
+            col_capa1, col_capa2 = st.columns(2)
+            with col_capa1:
+                multiselected_columns = st.multiselect('選擇要顯示的列',df_capa_show.columns)
+            with col_capa2:
+                chart_type=st.selectbox('選擇圖表類型',['折線圖','柱狀圖'])
+            
+            if multiselected_columns:
+                df_capa_selected = df_capa_show[multiselected_columns]
+                if chart_type == '折線圖':
+                    st.line_chart(df_capa_selected)
+                elif chart_type == '柱狀圖':
+                    st.bar_chart(df_capa_selected)
+            else:
+                if chart_type == '折線圖':
+                    st.line_chart(df_capa_show)
+                elif chart_type == '柱狀圖':
+                    st.bar_chart(df_capa_show)
+        with col_main2:
+
         
         #顯示數據
         show_raw= st.checkbox('顯示原始數據')
 
         if show_raw:
-            col1b, col2b = st.columns([2, 1])
+            col_caparaw1, col_caparaw2 = st.columns([2, 1])
 
-            with col1b:
+            with col_caparaw1:
                 sort_option= st.selectbox('排列方式',['年升序','年降序'])
                 if sort_option == '年升序':
-                    sored_data = df.sort_values(by='年',ascending=True)
+                    sored_data = df_capa.sort_values(by='年',ascending=True)
                 else:
-                    sored_data = df.sort_values(by='年',ascending=False)
+                    sored_data = df_capa.sort_values(by='年',ascending=False)
 
                 st.write(sored_data)
-            with col2b:
+            with col_caparaw2:
                 st.write('數據統計摘要')
-                st.write(df.describe())
+                st.write(df_capa.describe())
     with tab2:
         st.header("臺灣能源政策")
         with st.expander('前言'):
