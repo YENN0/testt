0f478451b5440a418bca1170fc6054102d574819
YENN0 <167049781+YENN0@users.noreply.github.com>
2024-06-04 21:26:11 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -22,8 +22,8 @@ def main():
         df = pd.read_csv('https://raw.githubusercontent.com/YENN0/testt/main/capacity%20classification.csv')
         df = df.rename(columns={'年':'index'}).set_index('index')
         st.subheader('裝置容量結構')
-        st.line_chart(df['年'])
-        st.bar_chart(df['年'])
+        st.line_chart(df)
+        st.bar_chart(df)
         #顯示數據
         show_raw= st.checkbox('顯示原始數據')
 
