34cee33beaca014b1ba41d6f730af84a60bac635
YENN0 <167049781+YENN0@users.noreply.github.com>
2024-06-05 11:55:48 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -59,7 +59,7 @@ def main():
             renewyear = st.slider("取得資料年份", 101, 113)
             renewtype = st.selectbox('選擇再生能源類型',['風力','太陽光電','其他(含水力)'])
             st.write(df_renew_show[df_renew_show['年別'] == renewyear])
-            df_renew_show = df_renew[renewtype]
+            #df_renew_show = df_renew[renewtype]
 
             Taiwan = {
                 "區域": ["基隆市", "台北市", "新北市", "桃園市", "新竹市", "新竹縣", "苗栗縣", "台中市", "彰化縣", "南投縣", "雲林縣", "嘉義市", "嘉義縣", "台南市", "高雄市", "屏東縣", "宜蘭縣", "花蓮縣", "台東縣", "澎湖縣", "金門縣", "連江縣"],
