99c524d7d88f6e32accbcf0296706715178fb217
YENN0 <167049781+YENN0@users.noreply.github.com>
2024-06-05 12:43:17 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -60,7 +60,6 @@ def main():
             renewtype = st.selectbox('選擇再生能源類型',['風力','太陽光電','其他(含水力)'])
             df_log=df_renew[df_renew['年別'] == renewyear]
             df_renewshow= df_log.loc[:,['縣市',renewtype]]
-            st.write(df_renewshow)
 
             Taiwan = {
                 "縣市": ['基隆市', '台北市', '新北市', '桃園市', '新竹市', '新竹縣', '苗栗縣', '台中市', '彰化縣', '南投縣', '雲林縣', '嘉義市', '嘉義縣', '台南市', '高雄市', '屏東縣', '宜蘭縣', '花蓮縣', '台東縣', '澎湖縣', '金門縣', '連江縣'],
@@ -68,10 +67,9 @@ def main():
                 "緯度": [25.138 , 25.063 , 25.067 , 24.916 , 24.800 , 24.820 , 24.473 , 24.162 , 23.962 , 23.872 , 23.728 , 23.485 ,23.446,23.161 ,22.745 ,22.473 ,24.552 ,23.778 ,22.903 ,23.568 ,24.465 ,26.154],
             }
             df_tw = pd.DataFrame(Taiwan)
-            st.write(df_tw)
             df_merged = pd.merge(df_renewshow, df_tw, on='縣市', how='inner')
             st.write(df_merged)
-            st.map(df_merged[['緯度', '經度']])
+            st.map(df_merged)
 
         with col_main4:
             st.subheader('各部門用電狀況')
